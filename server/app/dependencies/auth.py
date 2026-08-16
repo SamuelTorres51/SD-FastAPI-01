4b3d6ebe0871d59155e7a612512152dfd5fae829
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-08-16 19:35:09 -0300

    finalizando o cadastro e login com token

---
+++ server/app/dependencies/auth.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+from app.database.supabase import supabase
+
+
+security = HTTPBearer()
+
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    token = credentials.credentials
+
+    try:
+        response = supabase.auth.get_user(token)
+
+        if response.user is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Usuário não autenticado"
+            )
+
+        return response.user
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Token inválido ou expirado"
+        )
