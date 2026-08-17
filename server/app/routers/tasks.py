279a3b0e02c978999aa21bdd3c63a3932bc75491
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-08-17 12:43:11 -0300

    adicionando rota de update

--- server/app/routers/tasks.py
+++ server/app/routers/tasks.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app.database.supabase import get_supabase_client
 from app.dependencies.auth import get_current_user
-from app.schemas.task import TaskCreate
+from app.schemas.task import TaskCreate, TaskUpdate
 
 
 router = APIRouter(
@@ -71,4 +71,54 @@ def get_tasks(
         raise HTTPException(
             status_code=500,
             detail=f"Erro ao buscar tarefas: {str(e)}"
+        )
+
+
+
+# Endpoint Update Task
+
+@router.put("/{task_id}")
+def update_task(
+    task_id: str,
+    data: TaskUpdate,
+    auth=Depends(get_current_user)
+):
+    token = auth["token"]
+
+    supabase = get_supabase_client(token)
+
+    update_data = data.model_dump(
+        exclude_none=True,
+        by_alias=True
+    )
+
+    if "dueDate" in update_data:
+        update_data["due_date"] = update_data["dueDate"].isoformat()
+        del update_data["dueDate"]
+
+    try:
+        response = (
+            supabase
+            .table("tasks")
+            .update(update_data)
+            .eq("id", task_id)
+            .select("*")
+            .execute()
+        )
+
+        if not response.data:
+            raise HTTPException(
+                status_code=404,
+                detail="Tarefa não encontrada"
+            )
+
+        return response.data[0]
+
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Erro ao atualizar tarefa: {str(e)}"
         )
